3ff48342108f8b0541064503d13b2c9b30f4006f
Alejandro Sobrevilla <asobrevilla@msev.gob.mx>
2018-07-19 16:05:30 -0500

    Agrego un modelo básico de cliente

---
+++ src/SolidSlate/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SolidSlate.Models
+{
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public bool IsCompany { get; set; }
+    }
+}
